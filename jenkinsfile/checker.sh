a794e4f2181c53d2840549f4745a76f448fe364b
KH Hsu <kevinhsu@17.media>
2020-08-07 19:08:41 +0800

    push

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Please declare environment variables:
-# You may also declare them in ~/.slackrc file.
-
-
 
 main(){
     printenv
@@ -15,7 +11,8 @@ main(){
     STATUS=$?
     echo "i get $STATUS"
     echo "Did I send something?"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+    #curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+    bash jenkinsfile/slack.sh
 
 }
 
---
+++ jenkinsfile/slack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
