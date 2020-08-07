68b1bf52e4701d101c126206189fb0aed425eb12
KH Hsu <kevinhsu@17.media>
2020-08-07 19:02:01 +0800

    push

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 #Please declare environment variables:
-APP_SLACK_WEBHOOK="https://hooks.slack.com/services/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"
-#APP_SLACK_CHANNEL="kh-test"
-#APP_SLACK_USERNAME (optional)
-#APP_SLACK_ICON_EMOJI (optional)
 # You may also declare them in ~/.slackrc file.
 
 
@@ -19,72 +15,8 @@ main(){
     STATUS=$?
     echo "i get $STATUS"
     slack "Did I send something?"
-}
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
 
-init_params() {
-  # you may declare ENV vars in /etc/profile.d/slack.sh
-  if [ -z "${APP_SLACK_WEBHOOK:-}" ]; then
-    echo 'error: Please configure Slack environment variable: ' > /dev/stderr
-    echo '  APP_SLACK_WEBHOOK' > /dev/stderr
-    exit 2
-  fi
-
-  APP_SLACK_USERNAME=${APP_SLACK_USERNAME:-$(hostname | cut -d '.' -f 1)}
-
-  APP_SLACK_ICON_EMOJI=${APP_SLACK_ICON_EMOJI:-:slack:}
-  if [ -z "${1:-}" ]; then
-    echo 'error: Missed required arguments.' > /dev/stderr
-    echo 'note: Please follow this example:' > /dev/stderr
-    echo '  $ slack.sh "#CHANNEL1,CHANNEL2" Some message here. ' > /dev/stderr
-    exit 3
-  fi
-
-  slack_channels=(${APP_SLACK_CHANNEL:-})
-  if [ "${1::1}" == '#' ] || [ "${1::1}" == '@' ]; then
-    # explode by comma
-    IFS=',' read -r -a slack_channels <<< "${1}"
-    shift
-  fi
-  slack_message=${@}
-}
-
-
-send_message() {
-  local channel=${1}
-  echo 'Sending to '${channel}'...'
-  curl --silent --data-urlencode \
-    "$(printf 'payload={"text": "%s", "channel": "%s", "username": "%s", "as_user": "true", "link_names": "true", "icon_emoji": "%s" }' \
-        "${slack_message}" \
-        "${channel}" \
-        "${APP_SLACK_USERNAME}" \
-        "${APP_SLACK_ICON_EMOJI}" \
-    )" \
-    ${APP_SLACK_WEBHOOK} || true
-  echo
-}
-
-send_message_to_channels() {
-  for channel in "${slack_channels[@]:-}"; do
-    send_message "${channel}"
-  done
-}
-
-slack() {
-  # Set magic variables for current file & dir
-  __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-  readonly __dir __file
-
-  cd ${__dir}
-
-  if [ -f $(cd; pwd)/.slackrc ]; then
-    . $(cd; pwd)/.slackrc
-  fi
-
-  declare -a slack_channels
-
-  init_params ${@}
-  send_message_to_channels
 }
 
 main $@
