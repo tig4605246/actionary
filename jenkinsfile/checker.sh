93a27d49c2c49cf48aec503f8f92206cf1004b76
KH Hsu <kevinhsu@17.media>
2020-08-09 13:22:48 +0800

    push

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEMPLATE='{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Config commit:\n*<$COMMIT_URL|$COMMIT>*"}},{"type":"section","text":{"type":"mrkdwn","text":"*Result:*\nTests Passed\n*Commited By:*\n$COMMITER"}}]}'
+ERROR_TAGGING=',{"type":"section","text":{"type":"mrkdwn","text":"@sre"}}'
 
 main(){
     printenv
@@ -11,9 +13,14 @@ main(){
     STATUS=$?
     echo "i get $STATUS"
     echo "Did I send something?"
+    MSG=$( jq -n \
+                  --arg COMMIT_URL "https://github.com/tig4605246/actionary/commit/${GIT_COMMIT}" \
+                  --arg COMMIT "${GIT_COMMIT}" \
+                  --arg COMMITER "${GIT_COMMITER}"
+                  ${TEMPLATE} )
 #    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' ${SLACK}
-     curl -X POST -H 'Content-type: application/json' --data @jenkinsfile/payload.json ${SLACK}   
-
+#    curl -X POST -H 'Content-type: application/json' --data @jenkinsfile/payload.json ${SLACK}   
+    curl -X POST -H 'Content-type: application/json' --data ${MSG} ${SLACK}
 }
 
 main $@
