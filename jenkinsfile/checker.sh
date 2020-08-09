1ed040f384d6cd7612b7edc55cf48f983424eddf
KH Hsu <kevinhsu@17.media>
2020-08-09 15:02:38 +0800

    push

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#TEMPLATE="{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Result:*\nTests Passed\n*Commited By:*\n $GIT_COMMITER \"}}]}"
 ERROR_TAGGING=',{"type":"section","text":{"type":"mrkdwn","text":"@sre"}}'
 COMMITER_INFO=`git log  --pretty=format:'%an (%ae)' ${GIT_COMMIT}^! `
 
@@ -8,14 +7,13 @@ main(){
     echo "listing current dir"
     ls
     python --version
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Test Started:*\n*Commited By:*\n $COMMITER_INFO \"}}]}" ${SLACK}
     docker run --rm tig4605246/config-checker-python:latest python3 --version
     bash jenkinsfile/err.sh
     STATUS=$?
     echo "i get $STATUS"
     echo "commiter ${COMMITER_INFO}"
     echo "Did I send something?"
-    #curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$GIT_COMMIT | $GIT_COMMIT>*\"}" ${SLACK}
-#    curl -X POST -H 'Content-type: application/json' --data @jenkinsfile/payload.json ${SLACK}   
     curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Result:*\nTests Passed\n*Commited By:*\n $COMMITER_INFO \"}}]}" ${SLACK}
 }
 
