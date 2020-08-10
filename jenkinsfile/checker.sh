71d95d5604a5ea0427dc78798c42fb1c1de765b2
KH Hsu <kevinhsu@17.media>
2020-08-10 13:32:41 +0800

    test4

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -13,7 +13,7 @@ main(){
     STATUS=$?
     echo "i get $STATUS"
     echo "commiter ${COMMITER_INFO}"
-    echo "test3"
+    echo "test4"
     echo "Did I send something?"
     curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Result:*\nTests Passed\n*Commited By:*\n $COMMITER_INFO \"}}]}" ${SLACK}
 }
