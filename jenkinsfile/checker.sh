36459d628c14add1c219cd1330d85eb5d754ff05
KH Hsu <kevinhsu@17.media>
2020-08-11 12:47:26 +0800

    push

--- jenkinsfile/checker.sh
+++ jenkinsfile/checker.sh
@@ -14,6 +14,7 @@ main(){
     echo "i get $STATUS"
     echo "commiter ${COMMITER_INFO}"
     echo "Did I send something?"
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Tests of ${SCRIPT} failed*\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"},\"accessory\":{\"type\":\"image\",\"image_url\":\"https://api.slack.com/img/blocks/bkb_template_images/notificationsWarningIcon.png\",\"alt_text\":\"Warning\"}}${ERROR_TAGGING}]}" ${SLACK}
     curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Result:*\nTests Passed\n*Commited By:*\n $COMMITER_INFO \"}}]}" ${SLACK}
 }
 
